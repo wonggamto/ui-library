fbf2ba9667c9163a94f284f3e590269719adc439
黄锦涛 <7792275+shutiaojun@user.noreply.gitee.com>
2022-02-12 00:32:23 +0800

    topnav 样式更新，引入了全局样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo"></div>
-    <div class="menu"></div>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
   </div>
   <div class="banner">
     <h1>自制组件库</h1>
@@ -21,6 +24,21 @@ export default {
 
 <style lang='scss' scoped>
 .topnav {
-  border: 1px solid red;
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
 }
+
 </style>
