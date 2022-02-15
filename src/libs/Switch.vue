e710420b91cc1dc5159fa06bdc2c105b4192325e
黄锦涛 <7792275+shutiaojun@user.noreply.gitee.com>
2022-02-16 00:34:49 +0800

    Button 在 hover 时滚动效果实现

--- src/libs/Switch.vue
+++ src/libs/Switch.vue
@@ -9,25 +9,30 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-span{
-  $h:22px;
-  $h2:$h - 4px;
-  button{
-    height: $h;
-    width: $h * 2;
-    border:none;
-    background: blue;
-    border-radius: $h / 2;
-    position: relative;;
-  }
-  span{
-    position: absolute;
-    top:2px;
-    left:2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
-  }
+
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: blue;
+  border-radius: $h / 2;
+  position: relative;;
+}
+
+span {
+  position: absolute;
+  //display: inline-block;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
+
+button:hover >span {
+  left: calc(100% - #{$h2} - 2px);
 }
 </style>
